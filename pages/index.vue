a4818a6e3460965a9a1f2e28e8409e41c66794b9
b-jan <bapt.jan@gmail.com>
2018-04-04 01:36:45 +0200

    Remove doc links

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,6 @@
       <h2 class="subtitle">
         Cabinet du Docteur JAN - Chirurgien Dentiste
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
